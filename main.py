f0f5955ea100ed7e495b9fad762235ff314f5474
Tom Fox <kot3854@yandex.ru>
2024-06-13 12:22:44 +0300

    added a lot of function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import *
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import *
 from os import *
+import json as js
 
 #local classes
 from settings import *
@@ -46,17 +47,36 @@ def AllTablesGet():
                 return JSONResponse(content=tables)
         
         case _:
-            return JSONResponse(content=["TypeError: Unknown type of DB"], status_code=500)
+            return JSONResponse(content={"Error": "Unknown type of DB"}, status_code=500)
         
-    
+@app.post("/api/GetTableInfo")
+async def GetTableInfo(request:Request):
+    data = await request.json()
+    NameOfTable = data["name"]
+    match TypeOfDb:
+        case "sqlite3":
+                with engine.connect() as con:
+                    rs = con.execute(text(f"PRAGMA table_info({NameOfTable});"))
+                    print([row for row in rs])
+        case "postgres":
+            pass
+        case _:
+            return JSONResponse(content={"Error": "Unknown type of DB"}, status_code=500)
+  
 @app.post("/api/RawSQL")
-async def ExecuteRawSQL(request:Request):
+async def execute_raw_sql(request: Request):
     data = await request.json()
     query = data["query"]
-    with engine.connect() as con:
 
-        rs = await con.execute(query)
-        return JSONResponse(rs, status=200)
+
+    with engine.connect() as con:
+        rs = con.execute(text(query))
+        tables = [list(j) for j in rs]
+        system("cls")
+        print(tables)
+        return JSONResponse(content=tables)
+
+    
     
 #serving HTML views
     
--- static/main.js
+++ static/main.js
@@ -1,9 +1,11 @@
 var IsInputActive = false
 var CurrentTable = ""
+var CurrentOperation = ""
 
 const AddTablesToNavBar = async function() {
+    console.log("What you are looking for in console?")
     let elem = document.querySelector(".tables-navbar");
-    const response = await fetch(`http://${location.host}/api/GetAllTables`)
+    var response = await fetch(`http://${location.host}/api/GetAllTables`)
     let json = await response.json();
     json.forEach(element => {
 
@@ -19,16 +21,157 @@ const AddTablesToNavBar = async function() {
 
 
     });
+
+    CurrentTable = json[0]
+
+    console.log(CurrentTable)
 }
 
+const AddDataToTable = function(arr) {
+    table = document.querySelector(".")
+}
 
-//handling input submit
-let KbrdEvent = function(e) {
+const GetDataForTable = async function() {
 
-    if (e.key == "Enter" && IsInputActive) {
 
-    }
+    var response = await fetch(`http://${location.host}/api/GetAllTables`)
+    let h7JH = await response.json();
+
+    console.log("я ебу собак")
+    var DataTable = document.querySelector(".DataTable")
+    var SqlQuery = `SELECT * FROM ${h7JH[0]}`
+    console.log(CurrentTable)
+    console.log(SqlQuery)
+
+
+
+    var response = await fetch(`http://${location.host}/api/RawSQL`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        }, // Add headers to specify JSON content type
+        body: JSON.stringify({
+            query: SqlQuery,
+        }),
+    })
+    var data = await response.json();
+    data.forEach((arr) => {
+        var elementAAA = document.createElement("tr")
+        arr.forEach(
+            (DataValue) => {
+                var valueAAA = document.createElement("td")
+                valueAAA.innerHTML = DataValue
+                elementAAA.appendChild(valueAAA)
+
+            }
+        )
+        DataTable.appendChild(elementAAA)
+    })
 }
 
 //adding KeyBoard event to page
-window.addEventListener("keydown", KbrdEvent);
+window.addEventListener("keydown", async(e) => {
+    var elem = document.querySelector(".RawSQLInput");
+    var DataTable = document.querySelector(".DataTable")
+
+    DataTable.innerHTML = ""
+
+
+    if (IsInputActive && e.key === "Enter") {
+        console.log("sending request");
+        var response = await fetch(`http://${location.host}/api/RawSQL`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }, // Add headers to specify JSON content type
+            body: JSON.stringify({
+                query: elem.value,
+            }),
+        })
+
+        var data = await response.json();
+
+        data.forEach((arr) => {
+            var elementAAA = document.createElement("tr")
+            arr.forEach(
+                (DataValue) => {
+                    var valueAAA = document.createElement("td")
+                    valueAAA.innerHTML = DataValue
+                    elementAAA.appendChild(valueAAA)
+
+                }
+            )
+            DataTable.appendChild(elementAAA)
+        })
+
+
+
+
+
+
+
+    }
+});
+
+const ReadTable = async function(NameOfTable) {
+
+    console.log("я ебу собак")
+    var DataTable = document.querySelector(".DataTable")
+    var SqlQuery = `SELECT * FROM ${NameOfTable}`
+    console.log(CurrentTable)
+    console.log(SqlQuery)
+
+    DataTable.innerHTML = ""
+
+
+    var response = await fetch(`http://${location.host}/api/RawSQL`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        }, // Add headers to specify JSON content type
+        body: JSON.stringify({
+            query: SqlQuery,
+        }),
+    })
+    var data = await response.json();
+    data.forEach((arr) => {
+        var elementAAA = document.createElement("tr")
+        arr.forEach(
+            (DataValue) => {
+                var valueAAA = document.createElement("td")
+                valueAAA.innerHTML = DataValue
+                elementAAA.appendChild(valueAAA)
+
+            }
+        )
+        DataTable.appendChild(elementAAA)
+    })
+
+}
+
+const NewWindowCUD = async function(TypeOfOperation) {
+
+    CurrentOperation = TypeOfOperation
+
+    //creating windows
+    var NewDiv = document.createElement("div")
+    NewDiv.className = "pop-up-window"
+
+    NewDiv.innerHTML = `Insert data for ${CurrentOperation}`
+
+
+    // var response = await fetch(`${location.host}/api/GetTableInfo?name=${CurrentTable}`)
+    // var data = await response.json()
+    // var NumOfRow = data["NumOfRow"]
+
+    // for (var j = 0; j < NumOfRow; i++) {
+    //     var input = document.createElement("input")
+    //     NewDiv.appendChild(input)
+    // }
+
+
+    document.body.append(NewDiv)
+
+
+
+}
